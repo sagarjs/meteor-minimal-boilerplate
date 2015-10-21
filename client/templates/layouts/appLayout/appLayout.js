49c2895d98a318e88edb589c0bcf0d01616ebec3
Sagar Sodah <sagar.sodah@cmss.in>
2015-10-21 15:54:24 +0530

    Added boilerplate files and folders

---
+++ client/templates/layouts/appLayout/appLayout.js
@@ -0,0 +1,14 @@
+Meteor.startup(function() {
+	
+	/*
+	var id = Ghars.insert({title:"Sagar",loc:{type: "Point", coordinates:[0, 0]}}, function(e,r){
+		if (e != null)
+			alert(e);
+	});
+	
+  $('body').on('click', '[data-action=logout]', function(event) {
+    event.preventDefault();
+    AccountsTemplates.logout();
+  });
+  */
+});
